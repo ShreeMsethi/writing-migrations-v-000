c90fd03f1b9d83acd5f425c7c89a87ff6538c823
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2020-07-24 15:40:46 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   
   def change
-    add_column :student, :grade, :integer, :birthdate, :string
+    add_column :students, :grade, :integer, :birthdate, :string
   end
 
 end
